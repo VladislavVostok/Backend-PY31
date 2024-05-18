30330ea0b6d9fb3da52396f9627c5e018cf954fd
vladislav_vostok <vladislav.sage@yandex.ru>
2024-05-18 14:55:51 +0300

    hot fix

--- users/models.py
+++ users/models.py
@@ -6,7 +6,10 @@ class User(AbstractUser):
     username = models.CharField(unique=True, max_length=100)
     email = models.EmailField(unique=True)
     full_name = models.CharField(unique=True, max_length=100)
-    otp = models.CharField(max_length=100, null=True, blank=True)
+    
+    # OTP-код (one-time password) это одноразовый пароль, который 
+    # генерируют защищённые аппаратные устройства или программы
+    otp = models.CharField(max_length=100, null=True, blank=True)   
     refresh_token = models.CharField(max_length=1000, null=True, blank=True)
 
     USERNAME_FIELD = 'email'

707c6049b28ee673b91beef112727ac8cf9a3a6a
vladislav_vostok <vladislav.sage@yandex.ru>
2024-05-18 14:23:29 +0300

    Hot fix

--- users/urls.py
+++ users/urls.py
@@ -1,20 +1,10 @@
 from django.urls                    import path
-from .views                         import ( 
-                                                RegisterView, 
-                                                LoginView, 
-                                                UserView, 
-                                                LoginView
-                                            )
 from rest_framework_simplejwt.views import (
                                                 TokenObtainPairView,
                                                 TokenRefreshView,
                                             )
 
 urlpatterns = [
-    # path('register', RegisterView.as_view()),
-    # path('login', LoginView.as_view()),
-    # path('user', UserView.as_view()),
-    # path('user', LoginView.as_view()),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]

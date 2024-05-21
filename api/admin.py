b0d857353eb18cda3f2cec2ad1df13cb2d3834e5
petrachkov <petrachkov@obninsk.top-academy.local>
2024-05-21 21:13:31 +0300

    All

--- api/admin.py
+++ api/admin.py
@@ -1,2 +1,14 @@
 from    django.contrib  import  admin
+import    api.models  as api_models
 
+admin.site.register(api_models.Course)
+admin.site.register(api_models.Teacher)
+admin.site.register(api_models.Variant)
+admin.site.register(api_models.VariantItem)
+admin.site.register(api_models.Question_Answer)
+admin.site.register(api_models.Question_Answer_Message)
+admin.site.register(api_models.Cart)
+admin.site.register(api_models.CartOrder)
+admin.site.register(api_models.CartOrderItem)
+admin.site.register(api_models.Certificate)
+admin.site.register(api_models.Category)
